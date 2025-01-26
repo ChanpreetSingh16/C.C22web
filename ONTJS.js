b90dbcb3c283ce7c5f398e60c82e4235a2f4c0c4
ChanpreetSingh16 <schanpreet859@gmail.com>
2025-01-26 13:44:24 +0530

    Update ONTJS.js

--- ONTJS.js
+++ ONTJS.js
@@ -1,2 +1,10 @@
 function sfo() {
+	usercheck();
+}
+function usercheck() {
+	var id = document.getElementById("userID");
+	 if(id.value === "#chanr14a260125") {
+		 document.getElementById("specialId").style.display = "block";
+		 document.getElementById("usernotemain").innerHTML = "<div>Hello<br><font>26/01/2025 12:00(IST)</font></div>";
+	 }
 }
