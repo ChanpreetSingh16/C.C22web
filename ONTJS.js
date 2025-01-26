f6f9fcc7dea74b3e0427d185f0c1c603168a8327
ChanpreetSingh16 <schanpreet859@gmail.com>
2025-01-26 14:06:50 +0530

    Update ONTJS.js

--- ONTJS.js
+++ ONTJS.js
@@ -1,11 +1,30 @@
 function sfo() {
-	usercheck();
+	var id = localStorage.getItem("userid");
+	document.getElementById("userID").value = id;
+	if(id === "#chanr14a260125") {
+		 document.getElementById("specialId").style.display = "block";
+		 document.getElementById("normalId").style.display = "none";
+		 document.getElementById("usernotemain").innerHTML = "<div>Welcome Chanpreet Singh!</div>";
+		 //document.getElementById("usernotemain").innerHTML = "<div>Hello<br><font>26/01/2025 12:00(IST)</font></div>";
+	 }
+	else if(id === "#r14a260125") {
+		 document.getElementById("specialId").style.display = "block";
+		 document.getElementById("normalId").style.display = "none";
+		 document.getElementById("usernotemain").innerHTML = "<div>Welcome Mahika!</div>";
+	 }
+	else if(id === "sahaj260125") {
+		 document.getElementById("normalId").style.display = "block";
+		 document.getElementById("specialId").style.display = "none";
+		 document.getElementById("usernotemain").innerHTML = "<div>Welcome Sahajpreet Singh!</div>";
+	 }
+	 else{
+		 document.getElementById("specialId").style.display = "none";
+		 document.getElementById("normalId").style.display = "none";
+		 document.getElementById("usernotemain").innerHTML = "";
+	 }
 }
 function usercheck() {
 	var id = document.getElementById("userID");
-	 if(id.value === "#chanr14a260125") {
-		 document.getElementById("specialId").style.display = "block";
-		 document.getElementById("usernotemain").innerHTML = "<div>Hello<br><font>26/01/2025 12:00(IST)</font></div>";
-	 }
-	else{}
+	localStorage.setItem("userid", id.value);
+	sfo();
 }
