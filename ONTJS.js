777f55df5d9eee55471009f71f2fa5346e47855d
ChanpreetSingh16 <schanpreet859@gmail.com>
2025-04-06 10:03:38 +0530

    Update ONTJS.js

--- ONTJS.js
+++ ONTJS.js
@@ -92,8 +92,13 @@ function checkWebScor() {
 	}
 }
 function notifycate() {
-	document.getElementById("onweventmain").innerHTML = '<div>Packup! Getting the Update on Sun Apr 06 2025(India Standard Time)<br><br><font>Fri Apr 04 2025 15:21:57 GMT+0530 (India Standard Time)</font></div>';
-	document.getElementById("onweventmain").innerHTML += '<div>Logo of ONLINE Notes has been changed. You can download it for your shortcuts by the following link:- <a href="https://online2k23.github.io/ONLINE/onno2.png" target="_blank">New Logo</a> <br><br><font>Wed Feb 05 2025 18:23:46 GMT+0530 (India Standard Time)</font></div>';
+	if(localStorage.getItem("webscore") > 6){
+		document.getElementById("onweventmain").innerHTML = '<div>Website has been Updated! :)<br><br><font>Sun Apr 06 2025 10:03:15 GMT+0530 (India Standard Time)</font></div>';
+		document.getElementById("onweventmain").innerHTML += '<div>Logo of ONLINE Notes has been changed. You can download it for your shortcuts by the following link:- <a href="https://online2k23.github.io/ONLINE/onno2.png" target="_blank">New Logo</a> <br><br><font>Wed Feb 05 2025 18:23:46 GMT+0530 (India Standard Time)</font></div>';
+	}
+	else{
+	
+	}
 }
 	
 function usercheck() {
